2145c3d924ba75dea484b8748451337b35583d64
bragadeesh <bragu2004@gmail.com>
2024-12-28 00:12:16 +0530

    2nd commit

---
+++ hangman_game.py
@@ -0,0 +1,29 @@
+import random
+
+words = ["python", "java", "javascript", "ruby", "kotlin"]
+
+chosen_word = random.choice(words)
+word_display = ["_" for i in chosen_word]
+attempts = 8
+
+print("Welcome to Hangman!")
+
+while attempts > 0 and "_" in word_display:
+    print("\n"+" ".join(word_display))
+    guess = input("Guess a letter: ").lower()
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word):
+            if letter == guess:
+                word_display[index] = guess
+    else:
+        print("The letter doesn't appear in the word.. BOOOOOOO!!!!")
+        attempts -= 1
+        print(f"You have {attempts} attempts left!")
+
+if "_" not in word_display:
+    print("You guessed the word!")
+    print(" ".join(word_display))
+    print("You survived! :)")
+else:
+    print("You ran out of attempts.... The word was: ",chosen_word)
+    print("You lost... :(")
